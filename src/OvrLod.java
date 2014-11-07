a9d04cf4096fb01e89dc93b613ece21e5d55f144
Anand <anandabktda@gmail.com>
2014-11-07 15:39:57 +0530

    OverLoad Programs

---
+++ src/OvrLod.java
@@ -0,0 +1,23 @@
+
+public class OvrLod {
+
+	
+		OvrLod(int a,int b)
+	{
+		System.out.println("Sum is "+(a+b)); 
+
+	}
+	OvrLod(float a,float b)
+	{
+		System.out.println("Sum is "+(a+b));
+	}
+	OvrLod(String a,String b)
+	{
+		System.out.println("Sum is "+(a+b));
+
+	}
+	
+
+}
+
+
---
+++ src/OvrLodMain.java
@@ -0,0 +1,13 @@
+
+public class OvrLodMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		OvrLod ovr1=new OvrLod("An","And");
+		OvrLod ovr2=new OvrLod(20,30);
+		OvrLod ovr3=new OvrLod(14.59f,14.23f);
+	}
+
+}
